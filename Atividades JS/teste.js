e58cd1aa3ac90489c3b68f64bafebbafd69bba7a
Tiago Queiroz <tiago.dev.queiroz@gmail.com>
2023-05-18 12:19:24 -0300

    .

--- Atividades JS/teste.js
+++
@@ -1,3 +0,0 @@
-function teste(){
-    console.log("sla");
-}
--- Atividades c#/aluno.cs
+++ Atividades c#/aluno.cs
@@ -1,39 +1,14 @@
 using System;
-class cadastro{
+class aula6{
     static void Main(){
+        int abacate = 2;
+        int pera = 3;
 
-    string Nome;
-    char Sexo;
-    int Idade;
-    double Altura;
-
-    Console.WriteLine("Iniciando seu cadastro!");
-
-    Console.WriteLine("Digite seu nome:");
-    Nome=Console.ReadLine();
-
-    Console.WriteLine("Digite seu sexo F ou M:");
-    Sexo= char.Parse(Console.ReadLine());
-
-    Console.WriteLine("Digite sua idade:");
-    Idade= int.Parse(Console.ReadLine());
-
-    Console.WriteLine("Digite sua altura:");
-    Altura= double.Parse(Console.ReadLine());
-
-    Console.WriteLine("Suas informações:");
-    Console.WriteLine("Nome:" + Nome);
-    Console.WriteLine("Sexo:" + Sexo);
-    Console.WriteLine("Idade:" +Idade);
-    Console.WriteLine("Altura:"+ Altura);
-    Console.WriteLine("Informações cadastradas com sucesso!!");
-      
-
-   
-    
-
-
-
+        if(pera < abacate){
+            Console.WriteLine("pera é maior");
+        }else{
+            Console.WriteLine("abacate é maior");
+        }
 
     }
 }
