4ac12df9c1c80ce932e203fde3e19e9c3e033faa
Tiago Queiroz <tiago.dev.queiroz@gmail.com>
2023-07-05 12:34:43 -0300

    .

--- Atividades PW/academia/Index.js
+++ Atividades PW/academia/Index.js
@@ -14,6 +14,18 @@ ex.use(bp.json())
 ex.get("/",function(req,res){
     res.render('inicio')
 })
+ex.get("/contato",function(req,res){
+    res.render('contato')
+})
+ex.get("/horarios",function(req,res){
+    res.render('horarios')
+})
+ex.get("/quemsomos",function(req,res){
+    res.render('quemSomos')
+})
+ex.get("/cadastro",function(req,res){
+    res.render('cadastro')
+})
 
 ex.listen(666,function(){
     console.log('o host esta rodando na porta http://localhost:666');
